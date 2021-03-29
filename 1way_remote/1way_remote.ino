d12a8fc778ebfd8ec5d5f017d83623a4c5fead72
Pouya <43359396+OfficialPouya@users.noreply.github.com>
2021-03-29 16:53:09 -0500

    1way

--- 1way_remote/1way_remote.ino
+++ 1way_remote/1way_remote.ino
@@ -17,23 +17,17 @@ unsigned long lastTransmission;
 // typedef struct package Package;
 // Package dataTransmit;
 
-byte dead_man_pin = 5;
-byte red_led = 2;
-byte yellow_led = 3;
-byte green_led = 4;
+byte dead_man_pin = 9;
+byte red_led = 8;
+byte yellow_led = 7;
+byte green_led = 6;
+byte speaker = 5;
 int throttle = A1;
 char str[11]; 
 U8G2_SSD1306_128X32_UNIVISION_F_HW_I2C u8g2(U8G2_R0);
 
 void setup() {
-    // Serial.begin(115200);
-    u8g2.begin();
-    delay(1000);
-    pinMode(red_led, OUTPUT);
-    pinMode(yellow_led, OUTPUT);
-    pinMode(green_led, OUTPUT);
-    pinMode(dead_man_pin, INPUT_PULLUP);
-    myRadio.begin();  
+    // Serial.begin(115200);w3
     myRadio.setChannel(115); 
     myRadio.setPALevel(RF24_PA_MAX);
     myRadio.setDataRate(RF24_250KBPS);    
@@ -43,7 +37,7 @@ void setup() {
 
 void loop() {
     // read the value of POT
-    throttle = analogRead(A1);
+    throttle = analogRead(A3);
     // If dead man is triggered send value of POT, otherwise send
     // 512 (the middle val of potentiometer)
     if (digitalRead(dead_man_pin) == 0){throttle = throttle;}
@@ -55,13 +49,13 @@ void loop() {
 
 void transmit_to_board(){
     // Transmit once every 50 millisecond
-    if (millis() - lastTransmission >= 500) {
+    if (millis() - lastTransmission >= 1500) {
         lastTransmission = millis();
         digitalWrite(yellow_led, LOW);
         digitalWrite(red_led, LOW);
         digitalWrite(green_led, HIGH);
         // Transmit the speed value (0-1024).
-        Serial.print(throttle);
+//        Serial.print(throttle);
         myRadio.write(&throttle, sizeof(throttle));
     }
     else{
@@ -77,6 +71,11 @@ void update_screen(){
     digitalWrite(green_led, LOW);
     u8g2.clearBuffer();          // clear the internal memory
     u8g2.setFont(u8g2_font_ncenB08_tr); // choose a suitable font
-    u8g2.drawStr(0,10, "test");      // write speed value
+    u8g2.setCursor(0,22);
+    u8g2.print(throttle);
+    u8g2.setCursor(0,32);
+    u8g2.print(lastTransmission);
+    u8g2.setCursor(0,12);
+    u8g2.print(digitalRead(dead_man_pin));
     u8g2.sendBuffer();          // transfer internal memory to the display
 }
