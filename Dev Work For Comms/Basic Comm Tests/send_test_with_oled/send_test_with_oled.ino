f56b6b7eb546761206412110c9a426d9646d8242
Pouya (School) <pa2@illinois.edu>
2021-04-07 19:40:13 -0500

    added code for basic hardware test

---
+++ Dev Work For Comms/Basic Comm Tests/send_test_with_oled/send_test_with_oled.ino
@@ -0,0 +1,33 @@
+#include <SPI.h>
+#include <nRF24L01.h>
+#include <RF24.h>
+#include <U8g2lib.h>
+
+
+RF24 radio(9, 10); // CE, CSN
+
+const byte address[6] = "00001";
+unsigned long timer; 
+
+U8G2_SSD1306_128X32_UNIVISION_F_HW_I2C u8g2(U8G2_R0);
+
+void setup() {
+  u8g2.begin();
+  radio.begin();
+  radio.openWritingPipe(address);
+  radio.setPALevel(RF24_PA_MIN);
+  radio.stopListening();
+}
+
+void loop() {
+  timer = millis();
+  radio.write(&timer, sizeof(timer));
+  u8g2.clearBuffer();          // clear the internal memory
+  u8g2.setFont(u8g2_font_ncenB08_tr); // choose a suitable font
+  u8g2.setCursor(0,12);
+  u8g2.print("Time Sent: ");
+  u8g2.setCursor(0,22);
+  u8g2.print(timer);
+  u8g2.sendBuffer();          // transfer internal memory to the display
+  delay(1000);
+}
